55f95af90b9a46c1a839c0eb1b9c7258ca8581ac
Atharva Todkary <atharvatodkary@gmail.com>
2024-08-24 23:05:30 +0530

    Atharva commited

--- server/server.js
+++ server/server.js
@@ -1,4 +1,10 @@
+const { config } = require('dotenv');
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const WebSocket= require('ws');
+
+const URI = process.env.URI;
 
 const app = express();
 const PORT = process.env.port || 5000;
@@ -10,3 +16,20 @@ app.get('/', (req, res)=>{
     res.send('Welcome to NexTalk server');
 })
 
+const connectDB = async(req, res)=>{
+    try {
+        await mongoose.connect(URI);
+        console.log('Mongo Connected...')
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+}
+
+connectDB();
+
+// const ws = new WebSocket('ws://localhost:5000');
+// ws.on('error', console.log(error));
+// ws.on('open', ()=>{
+//     ws.send('Welcome to Websocket')
+// })
